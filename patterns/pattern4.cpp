54dd1b913afa947d07d5db3f60bd8c16246b02b8
melano <105978905+melqtx@users.noreply.github.com>
2024-05-23 13:28:07 +0530

    Update pattern4.cpp

--- patterns/pattern4.cpp
+++ patterns/pattern4.cpp
@@ -1,68 +1,17 @@
-/*
-printing this was pain in the ass
-but okay it was fun; my logic might suck, but atleast im transparent about it.
-
-
-5 5 5 5 5 5 5 5 5
-5 4 4 4 4 4 4 4 5
-5 4 3 3 3 3 3 4 5
-5 4 3 2 2 2 3 4 5
-5 4 3 2 1 2 3 4 5
-5 4 3 2 2 2 3 4 5
-5 4 3 3 3 3 3 4 5
-5 4 4 4 4 4 4 4 5
-5 5 5 5 5 5 5 5 5
-
-*/
-
-// add the above comment asshole
-#include <iostream>
-using namespace std;
-
 int main() {
 
   int n;
-  cout << n;
-  cin >> n;
+  std::cout << "enter n";
+  std::cin >> n;
 
-  for (int i = n; i > 0; i--) {
-
-    for (int j = n; j > i; j--) {
-
-      cout << j << " ";
+  // main loop of patterns start here
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      std::cout << "* ";
     }
-
-    for (int j = 0; j < (2 * i) - 1; j++) {
-
-      cout << i << " ";
-    }
-
-    for (int j = i; j < n; j++) {
-
-      cout << j + 1 << " ";
-    }
-
-    cout << endl;
-  }
-
-  for (int i = 0; i < n - 1; i++) {
-
-    for (int j = n; j > i + 2; j--) {
-
-      cout << j << " ";
-    }
-
-    for (int j = 0; j < (2 * i) + 3; j++) {
-
-      cout << i + 2 << " ";
-    }
-
-    for (int j = i + 3; j <= n; j++) {
-
-      cout << j << " ";
-    }
-
-    cout << endl;
+    std::cout << '\n';
   }
   return 0;
 }
+
+basic triangle
