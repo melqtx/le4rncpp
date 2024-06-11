87e0662cb03e1d1abc9a7ec9f8e77caeb8ee2687
melano <105978905+melqtx@users.noreply.github.com>
2024-06-11 11:41:44 +0530

    Update swap.cc

--- logics/swap.cc
+++ logics/swap.cc
@@ -1,28 +1,26 @@
-// C++ Program to demonstrate
-// Passing of references as parameters
+
 #include <iostream>
 using namespace std;
 
 // Function having parameters as
 // references
-void swap(int& first, int& second)
-{
-    int temp = first;
-    first = second;
-    second = temp;
+void swap(int &first, int &second) {
+  int temp = first;
+  first = second;
+  second = temp;
 }
 
-// Driver function
-int main()
-{
-    // Variables declared
-    int a = 2, b = 3;
+/
+  // Variables declared
+  int a, b;
+  cout << "enter nos to be swapped: ";
+  cin >> a >> b;
 
-    // function called
-    swap(a, b);
+  // function called
+  swap(a, b);
 
-    // changes can be seen
-    // printing both variables
-    cout << a << " " << b;
-    return 0;
+  // changes can be seen
+  // printing both variables
+  cout << a << " " << b;
+  return 0;
 }
