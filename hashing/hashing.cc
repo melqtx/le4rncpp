e50d3cf9f9d8e2b36da8d9b7f36049a883ee8ff8
melano <105978905+melqtx@users.noreply.github.com>
2024-06-13 11:49:00 +0530

    Update hashing.cc

--- hashing/hashing.cc
+++ hashing/hashing.cc
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int hashArray[100000000];  // 1e7 globally, if you wanna declare locally the limit is 1e6.
+
+int main() {
+  int n;
+  cout << "Enter n: ";
+  cin >> n;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  // precompute
+
+  for (int i = 0; i < n; i++) {
+    hashArray[arr[i]] += 1;
+  }
+
+  int q;
+  cout << "Enter queries: ";
+  cin >> q;
+  while (q--) {
+    int number;
+    cin >> number;
+    cout << "Number of " << number << " in the array: " << hashArray[number] << '\n';
+  }
+}
