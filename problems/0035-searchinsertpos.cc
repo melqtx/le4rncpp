886fcd9b0010cb0e4404562292346c751e9634f6
melano <105978905+melqtx@users.noreply.github.com>
2024-06-18 08:01:35 +0530

    Create 0035-searchinsertpos.cc

---
+++ problems/0035-searchinsertpos.cc
@@ -0,0 +1,20 @@
+//just do a simple binary search, instead of returning -1, return low.
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int h = nums.size() - 1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (target == nums[m]) {
+                return m;
+            } else if (target < nums[m]) {
+                h = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+};
