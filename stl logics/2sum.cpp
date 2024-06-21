3cbd177ab1ff22b17413f2be0ee0c8ed9dd648c8
mel <vaibhavmalik1127@gmail.com>
2024-06-21 16:16:30 +0530

    codes

--- stl logics/2sum.cpp
+++ stl logics/2sum.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-   vector<int>orgnum=nums;
-   sort(nums.begin(),nums.end());
-   int left=0;
-   int right=nums.size()-1;
-   while(left<right)
-   {
-    int sum=nums[left]+nums[right];
-    if(sum==target){
-    break;
-    }
-    else if (sum<target){
+  vector<int> twoSum(vector<int> &nums, int target) {
+    vector<int> orgnum = nums;
+    sort(nums.begin(), nums.end());
+    int left = 0;
+    int right = nums.size() - 1;
+    while (left < right) {
+      int sum = nums[left] + nums[right];
+      if (sum == target) {
+        break;
+      } else if (sum < target) {
         left++;
-    }
-    else{
+      } else {
         right--;
+      }
     }
-   }
-   vector<int>res;
-   for(int i=0;i<orgnum.size();i++)
-   {
-    if(orgnum[i]==nums[left]||orgnum[i]==nums[right])
-    {
+    vector<int> res;
+    for (int i = 0; i < orgnum.size(); i++) {
+      if (orgnum[i] == nums[left] || orgnum[i] == nums[right]) {
         res.push_back(i);
-        if(res.size()==2)
-        {
-            break;
+        if (res.size() == 2) {
+          break;
         }
+      }
     }
-   }
-   return res;
-    }
-
+    return res;
+  }
 };
