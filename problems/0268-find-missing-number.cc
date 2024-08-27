a43edbf707fc4f66b4a8813127784540c0092ea6
melano <105978905+melqtx@users.noreply.github.com>
2024-08-27 18:31:48 +0530

    Create 0268-find-missing-number.cc

---
+++ problems/0268-find-missing-number.cc
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+};
