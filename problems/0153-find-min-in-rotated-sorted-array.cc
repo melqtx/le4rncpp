ab3a2435d84633dfaa8c4d362d1f6717f0b187ca
melano <105978905+melqtx@users.noreply.github.com>
2024-09-15 13:54:23 +0530

    problem 153

--- problems/0153-find-min-in-rotated-sorted-array.cc
+++ problems/0153-find-min-in-rotated-sorted-array.cc
@@ -22,3 +22,30 @@ public:
         return ans;
     }
 };
+// consists of the same runtime, but slightly less comparisons.
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int ans = nums[0];
+        
+        while(low <= high) {
+            if(nums[low] <= nums[high]) {
+                ans = min(ans, nums[low]);
+                break;
+            }
+            
+            int mid = (low + high) / 2;
+            ans = min(ans, nums[mid]);
+            
+            if(nums[mid] < nums[high]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
