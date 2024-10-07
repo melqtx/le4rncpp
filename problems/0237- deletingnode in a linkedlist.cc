cce04ab9c0671a2042f03800647a455450c6dbf8
melano <105978905+melqtx@users.noreply.github.com>
2024-10-07 16:27:14 +0530

    weird ass ques tbh

--- problems/0237- deletingnode in a linkedlist.cc
+++ problems/0237- deletingnode in a linkedlist.cc
@@ -1,22 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-  void deleteNode(ListNode *node) {
-    // Check if the node to be deleted is the last node
-    if (node->next == nullptr) {
-      delete node;
-      return;
+    void deleteNode(ListNode* node) {        
+        node -> val = node -> next -> val;
+        
+        node -> next = node -> next -> next;
+        
     }
-
-    // Copy the value of the next node to the current node
-    node->val = node->next->val;
-
-    // Store the next node of the node to be deleted
-    ListNode *temp = node->next;
-
-    // Update the next pointer of the current node to skip the next node
-    node->next = node->next->next;
-
-    // Delete the next node
-    delete temp;
-  }
 };
