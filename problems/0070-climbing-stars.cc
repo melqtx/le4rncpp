e45be63070b32e8ab18d7a66e70e6435d6525099
melano <105978905+melqtx@users.noreply.github.com>
2024-10-08 11:25:23 +0530

    oke ig

--- problems/0070-climbing-stars.cc
+++ problems/0070-climbing-stars.cc
@@ -13,17 +13,19 @@ public:
 
 //somehow this doesnt pass all the test cases.
 //same as doing the fibonacchi appraoch and it works well;
-
-
 class Solution {
 public:
     int climbStairs(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if (n == 0 || n == 1) {
             return 1;
         }
         int one = 1, two = 1;
         for(int i = 2; i <= n; i++) {
             //int temp = one;
+            //one+= two; 
+            //two = temp; 
             one = one + two;
             two = one - two;
         }
