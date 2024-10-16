8d00d4c421ba10b21dc69abc52980c6302541d39
melano <105978905+melqtx@users.noreply.github.com>
2024-10-16 16:03:50 +0530

    koko

--- problems/1283-find-the-smallest-divisor-given-a-threshold.cc
+++ problems/1283-find-the-smallest-divisor-given-a-threshold.cc
@@ -1,6 +1,4 @@
-class Solution {
-public:
-    int smallestDivisor(vector<int>& nums, int threshold) {
+class Solution { public: int smallestDivisor(vector<int>& nums, int threshold) {
         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
         int low = 1;
@@ -23,3 +21,34 @@ public:
 };
 
 // THIS JUST WORKS. AND IDK HOW CAN I MAKE IT BETTER THAN THIS.
+//
+//
+//
+class Solution {
+public:
+    int calculateSum(vector<int>& nums, int divisor) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += (num + divisor - 1) / divisor;
+        }
+        return sum;
+    }
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low = 1;
+        int high = *max_element(begin(nums), end(nums));
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int sum = calculateSum(nums, mid);
+            
+            if (sum <= threshold) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        
+        return low;
+    }
+};
