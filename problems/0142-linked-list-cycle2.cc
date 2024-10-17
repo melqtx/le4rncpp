1df4e16e308e078a98c8c96c8f9ca39c8b15917b
melano <105978905+melqtx@users.noreply.github.com>
2024-10-17 21:08:00 +0530

    ll

--- problems/0142-linked-list-cycle2.cc
+++ problems/0142-linked-list-cycle2.cc
@@ -19,3 +19,32 @@ public:
         return nullptr;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
