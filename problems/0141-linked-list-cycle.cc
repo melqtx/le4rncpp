fb5f15fa509d56d6f5f02b8ec9027f173003c69c
melano <105978905+melqtx@users.noreply.github.com>
2024-10-17 20:39:16 +0530

    ll

--- problems/0141-linked-list-cycle.cc
+++ problems/0141-linked-list-cycle.cc
@@ -25,7 +25,22 @@ public:
         return (slow == fast);
     }
 };
-
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* p = head;
+        ListNode* q = nullptr;
+        ListNode *r = nullptr;
+        while(p){
+            r = q;
+            q = p;
+            p = p->next;
+            q->next = r;
+        }
+        head = q;
+        return head;
+    }
+};
 
 /**
  * Definition for singly-linked list.
