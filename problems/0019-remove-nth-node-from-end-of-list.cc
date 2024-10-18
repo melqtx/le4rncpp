95e1a793ee4cb7b51eee27fa3c47cdef02cce486
melano <105978905+melqtx@users.noreply.github.com>
2024-10-18 22:02:55 +0530

    ll

--- problems/0019-remove-nth-node-from-end-of-list.cc
+++ problems/0019-remove-nth-node-from-end-of-list.cc
@@ -26,3 +26,30 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        for(int i = 0; i < n; i++) fast = fast->next;
+        
+        if(fast == nullptr) {
+            ListNode* newHead = head->next;
+            delete head;
+            return newHead;
+        }
+        
+        while(fast->next != nullptr){
+            fast = fast->next;
+            slow = slow->next;
+        } 
+        
+        ListNode* toDelete = slow->next;
+        slow->next = slow->next->next;
+        delete toDelete;
+        
+        return head;
+    }
+};
