312935310324f582153b91a18e5db91ef65a8de8
melano <105978905+melqtx@users.noreply.github.com>
2024-10-18 22:28:46 +0530

    ll

--- problems/2095-delete-the-middle-of-a-linked-list.cc
+++ problems/2095-delete-the-middle-of-a-linked-list.cc
@@ -16,3 +16,26 @@ public:
         return head;
     }
 };
+
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head->next == nullptr) return head->next;
+        ListNode* fast = head;
+        ListNode* slow = head;
+        fast = fast->next->next;
+       // ListNode* prev = head;
+        while(fast != nullptr and fast->next != nullptr){
+            fast = fast->next->next;
+            //prev = slow;
+            slow = slow->next;
+        }
+        ListNode* del = slow->next;
+        slow->next = slow->next->next;
+        delete del;
+        //prev->next = slow->next;
+        //delete slow;
+        return head;
+    }
+};
