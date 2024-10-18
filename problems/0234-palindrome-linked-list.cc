eea7f9da4d9740885bdfea8e93f49aa7acec388c
melano <105978905+melqtx@users.noreply.github.com>
2024-10-18 20:28:03 +0530

    ll

--- problems/0234-palindrome-linked-list.cc
+++ problems/0234-palindrome-linked-list.cc
@@ -26,3 +26,50 @@ public:
         return true;
     }
 };
+
+
+
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* p = head;
+        ListNode* q = nullptr;
+        ListNode *r = nullptr;
+        while(p){
+            r = q;
+            q = p; 
+            p = p->next;
+            q->next = r;
+        }
+        head = q;
+        return head;
+    }
+
+
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return true;
+
+        // Find the end of first half and reverse second half
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* secondHalf = reverse(slow->next);
+        ListNode* firstHalf = head;
+
+        // Compare the two halves
+        while (secondHalf != nullptr) {
+            if (firstHalf->val != secondHalf->val)
+                return false;
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
+        }
+        slow->next = reverse(slow->next);
+
+        return true;
+    }
+};
