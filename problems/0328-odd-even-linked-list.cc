ddb1572d763c78887595780c7f5909ae84e4d2e0
melano <105978905+melqtx@users.noreply.github.com>
2024-10-18 21:30:18 +0530

    ll

--- problems/0328-odd-even-linked-list.cc
+++ problems/0328-odd-even-linked-list.cc
@@ -36,4 +36,26 @@ public:
     }
 };
 
-
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+        
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        
+        odd->next = evenHead;
+        
+        return head;
+    }
+};
