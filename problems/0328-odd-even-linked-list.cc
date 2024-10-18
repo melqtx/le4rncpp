71c0bc77bab50905e6d9b146423a482052a2f94f
melano <105978905+melqtx@users.noreply.github.com>
2024-10-18 20:59:11 +0530

    ll;

---
+++ problems/0328-odd-even-linked-list.cc
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        vector<int> a;
+        ListNode* temp = head;
+        while(temp != nullptr && temp->next != nullptr){
+            a.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if(temp) a.push_back(temp->val);
+        temp = head->next;
+        while(temp != nullptr && temp->next != nullptr){
+            a.push_back(temp->val);
+            temp = temp->next->next;
+        }
+        if(temp) a.push_back(temp->val);
+        int i = 0; 
+        temp = head;
+        while(temp){
+            temp->val = a[i];
+            i++;
+            temp = temp->next;
+        }
+        return head;
+    }
+};
+
+
