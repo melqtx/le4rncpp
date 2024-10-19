05d15318765da2103e888c095e2716f48fb52d8a
melano <105978905+melqtx@users.noreply.github.com>
2024-10-19 21:33:05 +0530

    ll

--- problems/0148-sort-list.cc
+++ problems/0148-sort-list.cc
@@ -28,3 +28,45 @@ public:
         return head;
     }
 };
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+        
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return mergeLists(left, right);
+    }
+    
+private:
+    ListNode* mergeLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        
+        if (l1) curr->next = l1;
+        if (l2) curr->next = l2;
+        
+        return dummy.next;
+    }
+};
