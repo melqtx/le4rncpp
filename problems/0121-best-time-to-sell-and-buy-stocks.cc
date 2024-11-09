b321ab726dabcc368484d1a5d42b5e1ddfb9564f
melano <105978905+melqtx@users.noreply.github.com>
2024-11-09 23:03:00 +0530

    oke ig

--- problems/0121-best-time-to-sell-and-buy-stocks.cc
+++ problems/0121-best-time-to-sell-and-buy-stocks.cc
@@ -32,3 +32,27 @@ public:
         return profit;
     }
 };
+
+
+// we could have solved this using kadanes algorithm as well.
+//
+// maximum subarray problem
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi = 0;
+        int min = INT_MAX;
+        
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+            else if (prices[i] > min) {
+                maxi = max(prices[i] - min, maxi);
+            }
+        }
+        
+        return maxi;
+    }
+};
