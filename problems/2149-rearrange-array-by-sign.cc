4a71912408948078c694d269a14a03ac24f17189
melano <105978905+melqtx@users.noreply.github.com>
2024-11-09 23:27:51 +0530

    oke ig

--- problems/2149-rearrange-array-by-sign.cc
+++ problems/2149-rearrange-array-by-sign.cc
@@ -17,3 +17,27 @@ public:
     return ans;
   }
 };
+
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos;
+        vector<int> neg;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] < 0) {
+                neg.push_back(nums[i]);
+            } else {
+                pos.push_back(nums[i]);
+            }
+        }
+        
+        for(int i = 0; i < nums.size()/2; i++) {
+            nums[2*i] = pos[i];
+            nums[2*i + 1] = neg[i];
+        }
+        
+        return nums;  
+    }
+};
