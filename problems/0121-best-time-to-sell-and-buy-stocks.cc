11cd785a3d06ce403643d2932e6ed111409ffd85
melano <105978905+melqtx@users.noreply.github.com>
2024-11-10 22:45:34 +0530

    121;

--- problems/0121-best-time-to-sell-and-buy-stocks.cc
+++ problems/0121-best-time-to-sell-and-buy-stocks.cc
@@ -16,3 +16,19 @@ public:
     return max_profit;
   }
 };
+
+class Solution {
+  :wq
+public:
+
+    int maxProfit(vector<int>& prices) {
+        int min = 0;
+        int profit = 0;
+        for(int i = 0; i <prices.size();i++){
+            if(prices[i]<prices[min])
+                min = i;
+            profit = max(profit,prices[i]-prices[min]);
+        }
+        return profit;
+    }
+};
