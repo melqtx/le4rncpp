0d8541bb44fe5dd680df90f1504af31049deacc3
melano <105978905+melqtx@users.noreply.github.com>
2024-12-02 11:56:27 +0530

    hehe

--- problems/0034-first-last-pos.cc
+++ problems/0034-first-last-pos.cc
@@ -41,4 +41,21 @@ public:
 
 
 //we couldve directly used the lb and up function, 
+
 //here but it wouldve consudmed more memory.
+// here comes the bruteforce bitch
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = -1, last=-1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == target){
+                if (first == -1) {
+                    first = i;
+                }
+                last = i;
+            }
+        }
+        return {first,last};
+    }
+};
