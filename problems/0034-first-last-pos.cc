53a9f3a801282dd29b2a56aef7e901a7b285cd5a
melano <105978905+melqtx@users.noreply.github.com>
2024-12-25 13:44:02 +0530

    fml

--- problems/0034-first-last-pos.cc
+++ problems/0034-first-last-pos.cc
@@ -55,3 +55,48 @@ public:
         return {first,last};
     }
 };
+
+//fuck my life, i literally forgor, the way i used to do this question man
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans = {-1, -1};
+        if(nums.empty()) return ans;
+        
+        // Find first position
+        int l = 0, h = nums.size() - 1;
+        while(l <= h) {
+            int m = l + (h-l)/2;
+            if(nums[m] == target) {
+                ans[0] = m;       
+                h = m - 1;
+            }
+            else if(nums[m] < target) {
+                l = m + 1;
+            }
+            else {
+                h = m - 1;
+            }
+        }
+        
+        if(ans[0] == -1) return ans;  // Target not found
+        l = ans[0]; 
+        h = nums.size() - 1;
+        while(l <= h) {
+            int m = l + (h-l)/2;
+            if(nums[m] == target) {
+                ans[1] = m;       
+                l = m + 1;         
+            }
+            else if(nums[m] < target) {
+                l = m + 1;
+            }
+            else {
+                h = m - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
