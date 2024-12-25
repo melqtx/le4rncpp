9feb549dc377cbb13cae81618dcdfd36985ad983
melano <105978905+melqtx@users.noreply.github.com>
2024-12-25 15:10:03 +0530

    fml

--- problems/0162-find-peak-element.cc
+++ problems/0162-find-peak-element.cc
@@ -27,3 +27,26 @@ auto init = []() {
 }();
 
 //weird ass question tbh.
+
+
+// i lowkey dont give a flying fuck
+// can we not use a simple condition? why complicate?
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        
+        if(nums[nums.size()-1] > nums[nums.size()-2]) 
+            return nums.size()-1;
+        
+        for(int i = 1; i < nums.size()-1; i++) {
+            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) {
+                return i;
+            }
+        }
+        
+        return 0;
+    }
+};
