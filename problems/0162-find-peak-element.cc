fee22953febbb2ea7413d7825fc34ae938f45013
melano <105978905+melqtx@users.noreply.github.com>
2024-12-25 15:28:25 +0530

    wagmi bros

--- problems/0162-find-peak-element.cc
+++ problems/0162-find-peak-element.cc
@@ -52,3 +52,28 @@ public:
 };
 
 //nvm this is slow as fuck
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+      // look this is simple, check for the first and last element seperately, then do a simple binary search
+        if(nums.size() == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+
+        if(nums[nums.size()-1] > nums[nums.size()-2]) 
+            return nums.size()-1;
+        
+        int low = 1, high = nums.size() - 2;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] >= nums[mid-1] && nums[mid] >= nums[mid+1]) return mid;
+            if(nums[mid] > nums[mid-1]){
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return 0;
+    }
+};
