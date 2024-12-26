1596ef7db9cdbee8c168330a5c6fe0596256dab2
melano <105978905+melqtx@users.noreply.github.com>
2024-12-26 15:14:12 +0530

    fuck me

--- problems/1283-find-the-smallest-divisor-given-a-threshold.cc
+++ problems/1283-find-the-smallest-divisor-given-a-threshold.cc
@@ -19,36 +19,36 @@ class Solution { public: int smallestDivisor(vector<int>& nums, int threshold) {
         return low;
     }
 };
-
+// im repeating one mistake again and again, taking low as zero, will cause division by zero error
+// do not mess up ther
 // THIS JUST WORKS. AND IDK HOW CAN I MAKE IT BETTER THAN THIS.
-//
-//
-//
 class Solution {
 public:
-    int calculateSum(vector<int>& nums, int divisor) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += (num + divisor - 1) / divisor;
+    int ans(vector<int>& nums, int divisor) {
+        int a=0;
+        for(int num: nums){
+            a+= (num + divisor - 1) / divisor;
+
         }
-        return sum;
+        return a;
     }
+           
 
     int smallestDivisor(vector<int>& nums, int threshold) {
+         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
         int low = 1;
-        int high = *max_element(begin(nums), end(nums));
-        
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            int sum = calculateSum(nums, mid);
-            
-            if (sum <= threshold) {
-                high = mid;
-            } else {
-                low = mid + 1;
+        int high = *max_element(nums.begin(), nums.end());
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            int sum = ans(nums,mid);
+            if(sum <= threshold){
+                high = mid - 1;
+            }else{
+                low = mid+1;
             }
         }
-        
         return low;
     }
 };
+
+//added another function, but the time complexity remains same;
