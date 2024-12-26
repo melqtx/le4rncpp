b47bcc379d1470b6051f9b992c7b38ba7c8f47b2
melano <105978905+melqtx@users.noreply.github.com>
2024-12-26 14:55:33 +0530

    wagmi bros

--- problems/1482-minimum-number-of-days-to-make-m-bouquets.cc
+++ problems/1482-minimum-number-of-days-to-make-m-bouquets.cc
@@ -39,4 +39,43 @@ private:
 
 //honestly i didnt get this approach initially, with the help of edutorial figured it out, this can me made 
 //better imma do it tomorrow i promies.:Wq
-//
+class Solution {
+public:
+    int bouquetcount(vector<int>& bloomDay,int k, int mid ){
+        int reqfl = k;
+        int bouq = 0;
+        int count = 0;
+        for(int flow: bloomDay){
+            if(flow <= mid){
+                count++;
+                if(count == k){
+                    bouq++;
+                    count = 0;
+                }
+            }else{
+                count=0;
+            }
+        }
+        return bouq;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        if(bloomDay.size()< m*k) return -1;
+
+        int low = ranges::min(bloomDay);
+        int high = ranges::max(bloomDay);
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            int count =  bouquetcount(bloomDay,k,mid);
+            if(count >= m){
+                high = mid -1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
+
+// there is a common pattern in these kind of questions, we gotta have two functions, one is the main one 
+// where we perform binary search, and the other one for calculating the answer.
