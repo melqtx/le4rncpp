c5d6bae9dd45d88fa9adef14fe59755ba4350123
v <vaibhavmalik1127@gmail.com>
2025-01-02 14:51:08 +0530

    fuck me

--- problems/2599-count-vowel-strings-in-ranges.cc
+++ problems/2599-count-vowel-strings-in-ranges.cc
@@ -1,15 +1,42 @@
 /*
  words = ["aba", "bcb", "ece", "aa", "e"]
-prefixSum = [1, 1, 2, 3, 4]  // cumulative count of valid words
+prefixsum = [1, 1, 2, 3, 4]  // cumulative count of valid words
 queries = [[0,2], [1,4]]  // example queries
 
-For query [0,2]:
-- Want count from index 0 to 2
-- prefixSum[2] - prefixSum[-1] = 2 - 0 = 2 valid words
+for query [0,2]:
+- want count from index 0 to 2
+- prefixsum[2] - prefixsum[-1] = 2 - 0 = 2 valid words
 
-For query [1,4]:
-- Want count from index 1 to 4
-- prefixSum[4] - prefixSum[0] = 4 - 1 = 3 valid words
+for query [1,4]:
+- want count from index 1 to 4
+- prefixsum[4] - prefixsum[0] = 4 - 1 = 3 valid words
  */ 
 
+class solution {
+public:
+    bool isvowel(char c) {
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    }
+    bool isvowelstring(string& word) {
+        return isvowel(word[0]) && isvowel(word[word.length() - 1]);
+    }
+    
+    vector<int> vowelstrings(vector<string>& words, vector<vector<int>>& queries) {
+        vector<int> answer;
+        
+        for(vector<int>& query : queries) {
+            int start = query[0];  
+            int end = query[1];    
+            int count = 0;  
+            for(int i = start; i <= end; i++) {
+                if(isvowelstring(words[i])) {
+                    count++;
+                }
+            }
+            answer.push_back(count);
+        }
+        return answer;
+    }
+};
 
+// would you believe me this fails just one test case, aaaaaaaaaaaaa fuck me
