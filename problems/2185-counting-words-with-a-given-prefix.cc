52771b3083679b129adf5258cc639b7f90fc3f32
v <vaibhavmalik1127@gmail.com>
2025-01-09 19:05:49 +0530

    lc daily

---
+++ problems/2185-counting-words-with-a-given-prefix.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count=0;
+        int preflen=pref.size();        
+        for(auto i:words){
+            if(i.substr(0,preflen) == pref)     
+                count++;                        
+        }
+        return count;
+        
+    }
+};
