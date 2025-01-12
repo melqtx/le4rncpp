ee92f70cdcfe37d9fb55996a2144614ff739f6ff
v <vaibhavmalik1127@gmail.com>
2025-01-12 22:32:21 +0530

    majority element

--- problems/0229-majority-element-2.cc
+++ problems/0229-majority-element-2.cc
@@ -44,3 +44,49 @@ public:
 // son of a bitch, this is n^2 fuck
 // a better Solution can be using a hashmap which we used in maj element 1
 // other one was moores algorithm
+
+// gotta hail moores voting algorithm, well there were two things
+// we did differently than the last 
+// first we had to return an array basically vector
+// and this time there could be two majority element,
+// we take two counters this time,
+// thing to keep in mind these two counters do not contains dub so we do it like that 
+// at the end, just check if there exists a majority element
+
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0;
+        int v1 = INT_MIN;
+        int v2 = INT_MIN;
+        int cnt2 = 0;
+
+
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt1 == 0 && nums[i] != v2){
+                cnt1=1;
+                v1 = nums[i];
+            }else if(cnt2 == 0 && nums[i] != v1) {
+                cnt2 = 1;
+                v2 = nums[i];
+            }else if(nums[i] == v1) cnt1++;
+            else if(nums[i] == v2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        vector<int> ls;
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(v1 == nums[i]) cnt1++;
+            if(v2 == nums[i]) cnt2++;
+        }
+        int mini = (int)(nums.size()/3) +1;
+        if(cnt1 >= mini) ls.push_back(v1);
+        if(cnt2 >= mini) ls.push_back(v2);
+        sort(ls.begin(), ls.end());
+        return ls;
+    }
+};
