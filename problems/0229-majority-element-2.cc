8b21a449ec743b88e065529319ea6dc894d2e1a7
v <vaibhavmalik1127@gmail.com>
2025-01-12 16:39:16 +0530

    0229

---
+++ problems/0229-majority-element-2.cc
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> result;
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            //to not get dubs in the result
+            if(find(result.begin(), result.end(), nums[i]) != result.end())
+                continue;
+            int count = 0;
+            for(int j = 0; j < n; j++) {
+                if(nums[i] == nums[j])
+                    count++;
+            }
+            if(count > n/3)
+                result.push_back(nums[i]);
+        }
+        return result;
+    }
+};
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        unordered_set<int> result;  
+        int n = nums.size();
+        
+        for(int i = 0; i < n; i++) {
+            if(result.count(nums[i]) > 0)
+                continue;
+            int count = 0;
+            for(int j = 0; j < n; j++) {
+                if(nums[i] == nums[j])
+                    count++;
+            }
+            
+            if(count > n/3)
+                result.insert(nums[i]);
+        }
+        return vector<int>(result.begin(), result.end());
+    }
+};
