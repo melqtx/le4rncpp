d11627f10de6542d5f834e5b442709bcbb45ccda
v <vaibhavmalik1127@gmail.com>
2025-04-10 19:55:32 +0530

    lc

--- problems/0128-longest-consecutive-sequence.cc
+++ problems/0128-longest-consecutive-sequence.cc
@@ -48,12 +48,45 @@ public:
   }
 };
 
+
+end() returns an iterator pointing to the position just after the last element in the container
+It doesn't point to any actual element (it's a "past-the-end" iterator)
+It's commonly used as a sentinel value to indicate "not found" or the end of iteration
+When comparing with find(), the expression find(value) == end() means "the value was not found in the set"
 /*
     Given unsorted array, return length of longest consecutive sequence
-    Ex. nums = [100,4,200,1,3,2] -> 4, longest is [1,2,3,4]
+    Ex. nums = [100,4,200,1,3,2] -> 4, longest is [1,
+
+end() returns an iterator pointing to the position just after the last element in the container
+It doesn't point to any actual element (it's a "past-the-end" iterator)
+It's commonly used as a sentinel value to indicate "not found" or the end of iteration
+When comparing with find(), the expression find(value) == end() means "the value was not found in the set"2,3,4]
 
     Store in hash set, only check for longer seq if it's the beginning
 
     Time: O(n)
     Space: O(n)
 */
+
+// i read cses last week, so made the code more efficient for cp 
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(0);
+        unordered_set<int> st(nums.begin(), nums.end());
+        if(nums.size() == 0) return 0;
+        int longest = 1;
+        for(auto it: st) {
+            if(st.find(it - 1) == st.end()){
+                int cnt = 1; 
+                while(st.find(it + cnt) != st.end()) {
+                    cnt = cnt + 1;
+                }
+                longest = max(cnt,longest);
+            }
+        }
+        return longest;
+    }
+};
